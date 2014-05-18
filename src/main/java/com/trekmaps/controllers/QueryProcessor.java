d13f3067ecee271ea0ca4ddfe690189bbdb9e7d3
sachpa <sachinpatil41286@gmail.com>
2014-05-18 20:38:31 +0530

    Adding Query Processor

---
+++ src/main/java/com/trekmaps/controllers/QueryProcessor.java
@@ -0,0 +1,44 @@
+package com.trekmaps.controllers;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+@Path("data/places")
+public class QueryProcessor {
+
+	  @GET
+	  @Path("all")
+	  @Produces(MediaType.APPLICATION_JSON)
+	  public String queryUsers() {
+
+		  String result = "{ name : \"Sachin\", surname : \"Patil\" }" ;
+
+		  return result;
+	  }
+	  
+/*	  @GET
+	  @Path("places")
+	  @Produces(MediaType.APPLICATION_JSON)
+	  public String queryPlaces(@QueryParam("type") String type )
+	  {
+		  
+		  if ( null == type || type.isEmpty()  )
+		  {
+			  return "[]" ;
+		  }
+		  
+		  if ( type.equals("All") )
+		  {
+			  type = "" ;
+		  }
+		  
+		  SelectQuery query = new SelectQuery();
+		  String result = query.getPlaces(type);
+		  
+		  return result ;
+	  }
+*/
+}
